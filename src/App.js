b2afe2b7b125f2bc9b9587fb29fbc1dfd03566a7
wakii <cwh6997@gmail.com>
2019-08-23 23:19:32 +0900

    installed  Axios, fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: []
+  };
+
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
   };
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 5000);
+    this.getMovies();
   }
 
   render() {
     const { isLoading } = this.state;
-
     return <div> {isLoading ? "Loading" : "ready"}</div>;
   }
 }
