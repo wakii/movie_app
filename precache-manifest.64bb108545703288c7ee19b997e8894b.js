3578f6a1d63682edbf3276a520fb39654d28e457
wakii <cwh6997@gmail.com>
2019-08-26 22:19:42 +0900

    Updates

---
+++ precache-manifest.64bb108545703288c7ee19b997e8894b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ab8100cd4098c25d619b73affccfca",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "df76b3fe48e24037b102",
+    "url": "/movie_app/static/css/main.59ce8b4d.chunk.css"
+  },
+  {
+    "revision": "35553d95505140ba87a3",
+    "url": "/movie_app/static/js/2.e5148252.chunk.js"
+  },
+  {
+    "revision": "df76b3fe48e24037b102",
+    "url": "/movie_app/static/js/main.13e4558d.chunk.js"
+  },
+  {
+    "revision": "5b6a134c98cd9d238fba",
+    "url": "/movie_app/static/js/runtime~main.d0c568e7.js"
+  }
+]);
